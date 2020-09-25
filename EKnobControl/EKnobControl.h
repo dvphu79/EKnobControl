53da0f91dd97d7beb4a1b4404ca532ee509e08c7
Do Van Phu <dvphu79@gmail.com>
2020-09-25 16:57:44 +0700

    create project

---
+++ EKnobControl/EKnobControl.h
@@ -0,0 +1,19 @@
+//
+//  EKnobControl.h
+//  EKnobControl
+//
+//  Created by Do Van Phu on 9/25/20.
+//  Copyright © 2020 Ekino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EKnobControl.
+FOUNDATION_EXPORT double EKnobControlVersionNumber;
+
+//! Project version string for EKnobControl.
+FOUNDATION_EXPORT const unsigned char EKnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EKnobControl/PublicHeader.h>
+
+
